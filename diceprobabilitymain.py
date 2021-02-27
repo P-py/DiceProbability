c3c6e119826add946a3323e79a3a8864da578945
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-02-27 16:11:20 -0300

    Update diceprobabilitymain.py

--- diceprobabilitymain.py
+++ diceprobabilitymain.py
@@ -1,10 +1,13 @@
-import matplotlib.pyplot
-import random
-import time
+#Importing libraries
+import matplotlib.pyplot #pip install matplotlib
+import random #Natibe lib
+import time #Native lib
 
-numTentativas = int(input('Quantas vezes deseja realizar lançamentos?'))
+#Asking for input
+numTentativas = int(input('[EN] How many times do you wanna roll the dices? / [PT-BR] Quantas vezes deseja jogar os dados?')) 
 totalLancaçamentos = numTentativas
 
+#Defining the function and the loop
 def probabilidade():
     tentativas = 1
     dadosIguais = 0
@@ -13,13 +16,14 @@ def probabilidade():
         d2 = random.randint(1,6)
         if d1 == d2:
             dadosIguais += 1
-            print(f'DADOS IGUAIS! ({d1}, {d2})')
+            print(f'[PT-BR] DADOS IGUAIS! / [EN] EQUAL DICES! - ({d1}, {d2})')
         else:
-            print(f'LANÇAMENTO NORMAL! ({d1}, {d2})')
+            print(f'[PT-BR] LANÇAMENTO NORMAL! / [EN] NON EQUAL DICES! - ({d1}, {d2})')
         tentativas += 1
-        #time.sleep(.1)
-    matplotlib.pyplot.bar('Quantidade de lançamentos iguais', dadosIguais)
-    matplotlib.pyplot.bar('Quantidade total de lançamentos', numTentativas)
-    matplotlib.pyplot.show()
+        #time.sleep(.1) - You can use that if you want a nice interval of between results
+    matplotlib.pyplot.bar('[EN] Number of equal dices / [PT-BR] Números de lançamentos iguais', dadosIguais)
+    matplotlib.pyplot.bar('[EN] Total number of launches / [PT-BR] Número total de lançcamentos', numTentativas)
+    matplotlib.pyplot.show() 
+    #Showing the graphs/results 
 probabilidade()
         
